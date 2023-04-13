c1a0acb9c5291ae5e233b074590de5862561bd55
Katherine.Pedreira <kakamarpe@gmail.com>
2023-04-13 13:23:25 -0300

    adding layout and creating function for requesting the number of students students

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(onInit());
+
+function onInit() {
+    $.ajax({
+        url: "http://localhost:8080/api/aluno/total",
+        type: "get",
+        dataType: "json",
+        success: function(res) {
+           $("#div-total-alunos").html(res);
+        }
+    });
+}
+
+
+  
